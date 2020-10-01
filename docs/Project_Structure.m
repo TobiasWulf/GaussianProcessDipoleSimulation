619afbd7f76f855d6cf3b3945c2cbfcba41619c8
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2020-10-01 15:14:20 +0200

    Add empty doc file to describe project structure.

---
+++ docs/Project_Structure.m
@@ -0,0 +1,12 @@
+%% Project Structure
+%
+%
+% Created on October 1. 2020 by Tobias Wulf. Copyright Tobias Wulf 2020.
+%
+% <html>
+% <!--
+% Hidden Clutter.
+% Edited on Month DD. YYYY by Editor: Single line description.
+% -->
+% </html>
+%
