471be104203a9f0d1cd861be359fa4d27cae07b4
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2020-10-02 11:41:05 +0200

    Rename Flows.m to Work_Flows.m for wider flow entries under that chapter.

--- docs/Work_Flows.m
+++ docs/Work_Flows.m
@@ -1,4 +1,4 @@
-%% Flows
+%% Work_Flows
 %
 %
 % Created on September 21. 2020 by Tobias Wulf. Copyright Tobias Wulf 2020.
@@ -8,6 +8,7 @@
 % Hidden Clutter.
 % Edited on Month DD. YYYY by Editor: Single line description.
 % Edited on September 30. 2020 by Tobias Wulf: Add Project Preparation section.
+% Edited on October 02. 2020 by Tobias Wulf: Rename file to Work_Flows.m
 % -->
 % </html>
 %
