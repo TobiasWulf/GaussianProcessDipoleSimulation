87cff6b9d8fc65a09584021958d96c67e30cdbad
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2020-10-10 09:35:41 +0200

    Add Documentation Workflow m-file to doc folder.

---
+++ docs/Documentation_Workflow.m
@@ -0,0 +1,12 @@
+%% Documentation Workflow
+%
+%
+% Created on October 10. 2020 by Tobias Wulf. Copyright Tobias Wulf 2020.
+%
+% <html>
+% <!--
+% Hidden Clutter.
+% Edited on Month DD. YYYY by Editor: Single line description.
+% -->
+% </html>
+%
