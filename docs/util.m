1f818bedc642a2745288400aa0c89ebf8bc480fc
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2020-11-04 11:53:31 +0100

    Rename accrording to guidlines.

---
+++ docs/util.m
@@ -0,0 +1,32 @@
+%% util
+% The main property of this module is to contain functions and classes which are
+% used in different scenarios or reused in different modules. So they are 
+% providing a more general use case and not a specific one e.g. like a certain
+% algebra function that almost or allways computes the same use case. The util
+% classifficated source code solve module unrelated tasks.
+%
+%
+%% removeFilesFromDir
+% Remove files from passed directory and identifier. Return a operation status
+% if files are removed successful or not.
+%
+%
+%% publishFilesFromDir
+% Publish m-files with Matlab built-in publish mechanism, scan m-files from
+% directory recursively.
+%
+%
+%% plotFunctions
+% A submodule to contain plot functions for general and specific use on data or
+% datasets.
+%
+% Created on October 10. 2020 by Tobias Wulf. Copyright Tobias Wulf 2020.
+%
+% <html>
+% <!--
+% Hidden Clutter.
+% Edited on October 10. 2020 by Tobias Wulf: Add removeFilesFromDir.
+% Edited on October 24. 2020 by Tobias Wulf: Add plotFunctions submodule.
+% -->
+% </html>
+%
