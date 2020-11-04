11fc000b1b56c394244f33c8c496640d6bf90585
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2020-11-04 18:12:37 +0100

    Add rotate3DVector function.

---
+++ docs/sensorArraySimulation.m
@@ -0,0 +1,17 @@
+%% sensorArraySimulation
+%
+%
+%% rotate3DVector
+% Rotate a vector with x-, y- and z-components in a 3D-coordinate system. Rotate
+% one step of certain angles.
+%
+%
+% Created on November 04. 2020 by Tobias Wulf. Copyright Tobias Wulf 2020.
+%
+% <html>
+% <!--
+% Hidden Clutter.
+% Edited on November 04. 2020 by Tobias Wulf: Add rotate3DVector.
+% -->
+% </html>
+%
