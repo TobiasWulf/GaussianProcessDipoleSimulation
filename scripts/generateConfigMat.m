b88819b418415f3c217da2b223611978be9152a6
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2020-11-09 21:32:15 +0100

    Add configuration for sensor array generation and dipole simulation to generate training and test datasets for gaussian process.

--- scripts/generateConfigMat.m
+++ scripts/generateConfigMat.m
@@ -23,6 +23,7 @@
 % Hidden Clutter.
 % Edited on Month DD. YYYY by Editor: Single line description.
 % Edited on November 09. 2020 by Tobias Wulf: Save PathVariables as struct.
+% Edited on November 09. 2020 by Tobias Wulf: Add Options for dipole simulation with square sensor array.
 % -->
 % </html>
 %
@@ -35,6 +36,14 @@ clearvars;
 clc;
 
 
+%% GeneralOptions
+% General options like formats for strings or date or anything else what
+% has no special relation to a theme complex.
+disp('Set general options ...');
+GeneralOptions = struct;
+GeneralOptions.dateFormat = 'yyyy-mm-dd_HH-MM-SS-FFF';
+
+
 %% Path Variables
 % Key path variables and directories, often used in functions or scripts.
 % Collet the path in a struct for easier save the struct fields as variables to
@@ -45,7 +54,8 @@ disp('Create current project instance to gather information ...');
 % create current project instance to retrieve root information
 projectInstance = matlab.project.currentProject;
 
-disp('Create path variables ...');
+disp('Set path variables ...');
+PathVariables =struct;
 
 % project root path, needs to be recreated generic to work on different machines
 PathVariables.rootPath = projectInstance.RootFolder;
@@ -55,7 +65,7 @@ PathVariables.dataPath = fullfile(PathVariables.rootPath, 'data');
 
 % path to TDK TAS2141 TMR angular sensor characterization dataset
 PathVariables.tdkDatasetPath = fullfile(PathVariables.dataPath, ... 
-    'TDK_TAS2141_Characterization_2019-07-24.mat');
+    'TDK_TAS2141_Characterization_2020-10-22_18-12-16-827.mat');
 
 % path to config file dataset
 PathVariables.configPath = fullfile(PathVariables.dataPath, 'config.mat');
@@ -110,6 +120,8 @@ PublishOptions.showCode = true;
 % certain configurations. Use options with unique create functions for fast
 % forward plotting routines. See Matlab documentation for further figure
 % options if needed.
+disp('Set figure and tile options for unified plots ...');
+FigureOptions = struct;
 FigureOptions.NumberTitle = 'off';
 FigureOptions.Units = 'normalized';
 FigureOptions.WindowStyle = 'docked';
@@ -119,22 +131,135 @@ TileOptions.Padding = 'normal';
 TileOptions.TileSpacing = 'compact';
 
 
+%% Sensor Array Options
+% The options control the build up of the sensor array in geometry and
+% techincal behavior. This means number of sensors in the array and its
+% size in mm. The supply and offset voltage of each sensor which is needed
+% for using the characterization which is normed in mV/V.
+disp('Set sensor array option for geometry and behavior ...');
+SensorArrayOptions = struct;
+
+% Geometry of the sensor array
+SensorArrayOptions.geometry = 'square';
+
+% Sensor array square dimension
+SensorArrayOptions.dimension = 8;
+
+% Sensor array size in mm
+SensorArrayOptions.size = 2;
+
+% Sensor array simulated supply voltage in volts
+SensorArrayOptions.vcc = 3.3;
+
+% Sensor array simulated offset voltage for bridge outputs in volts
+SensorArrayOptions.voff = 1.65;
+
+% Senor array voltage norm factor to recalculate norm bridge outputs to
+% given supply voltage and offset voltage, current normin is mV/V which
+% implements factor of 1e3
+SensorArrayOptions.vnorm = 1e3;
+
+
+%% Dipole Options
+% Dipole options to calculate the magnetic field which stimulate the sensor
+% array. The dipole is gained to sphere with additional z distance to the
+% array by sphere radius.
+disp('Set dipole options to calculate magnetic stimulus ...');
+DipoleOptions = struct;
+
+% Radius in mm of magnetic sphere in which the magnetic dipole is centered.
+% So it can be seen as z-offset to the sensor array.
+DipoleOptions.radius = 2;
+
+% H-field magnitude to multiply of generated and relative normed dipole
+% H-fields, the norming is done in zero position of [0 0 z+offset] for
+% 0° due to the position of the magnetic moment [-1 0 0] x and y components
+% are not relevant, norming without tilt. Magnitude in kA/m
+DipoleOptions.hMag = 8.5;
+
+% Magnetic moment magnitude attach rotation to the dipole field at a
+% certain position with x, y and z components. Choose a huge value to
+% prevent numeric failures
+DipoleOptions.mMag = 1e6;
+
+
+%% Traning Options
+% Training options gives the software the needed information to generate
+% training datasets by the sensor array simulation with a dipole manget as
+% stimulus which pushed with an z offeset to a sphere.
+disp('Set training options to generate dataset ...');
+TrainingOptions = struct;
+
+% Sensor array relative position to dipole magnet as position vector with
+% x, y and z posiotn in mm. Negative x for left shift, negative y for up
+% shift and negative z to place the layer under the dipole decrease z to
+% increase the distance. The z-position will be subtracted by dipole sphere
+% radius in simulation. So there is an offset given by the sphere radius
+TrainigOptions.xPos = [0,];
+TrainigOptions.yPos = [0,];
+TrainigOptions.zPos = [0,];
+
+% Dipole tilt in z-axes
+TrainigOptions.tilt = [0,];
+
+% Resolution of rotaion in degree, use same resoultion in training and test
+% datasets to have the ability to back reference the index to fullscale
+% test data sets 
+TrainingOptions.angleRes = 0.5;
+
+% Number rotaion angles, even distribute between 0° and 360° with respect
+% to the resolution, even down sampling. To generate full scale the number
+% relatead to the resolution or fast generate but wrong number to 0 to
+% generate full scale rotation.
+TrainingOptions.nAngles = 7;
+
+
+%% Test Options
+% Test options gives the software the needed information to generate
+% test datasets by the sensor array simulation with a dipole manget as
+% stimulus which pushed with an z offeset to a sphere.
+disp('Set test options to generate dataset ...');
+TestOptions = struct;
+
+% Sensor array relative position to dipole magnet as position vector with
+% x, y and z posiotn in mm. Negative x for left shift, negative y for up
+% shift and negative z to place the layer under the dipole decrease z to
+% increase the distance. The z-position will be subtracted by dipole sphere
+% radius in simulation. So there is an offset given by the sphere radius
+TestOptions.xPos = [0,];
+TestOptions.yPos = [0,];
+TestOptions.zPos = [0, 1, 2, 3, 4];
+
+% Dipole tilt in z-axes
+TestOptions.tilt = [0,];
+
+% Resolution of rotaion in degree, use same resoultion in training and test
+% datasets to have the ability to back reference the index to fullscale
+% test data sets 
+TestOptions.angleRes = 0.5;
+
+% Number rotaion angles, even distribute between 0° and 360° with respect
+% to the resolution, even down sampling. To generate full scale the number
+% relatead to the resolution or fast generate but wrong number to 0 to
+% generate full scale rotation.
+TestOptions.nAngles = 720;
+
+
 %% Save Configuration
 % Save section wise each config part as struct or struct fields to standalone
 % variables in config.mat use newest save format with no compression.
 
-% save PathVariables struct to config.mat with no compression
-disp('Save path variables configuration to config.mat ...');
-save(PathVariables.configPath, 'PathVariables', ...
+% create config.mat with timestamp of creation
+disp('Create config.mat ...');
+timestamp = datestr(now, GeneralOptions.dateFormat);
+save(PathVariables.configPath, ...
+    'timestamp', ...
+    'PathVariables', ...
+    'PublishOptions', ... 
+    'FigureOptions', ...
+    'TileOptions', ... 
+    'SensorArrayOptions', ... 
+    'DipoleOptions', ...
+    'TrainingOptions', ... 
+    'TestOptions', ...
     '-v7.3', '-nocompression');
-
-% save PublishOptions struct to config.mat with no compression, append to file
-disp('Save publish options configuration to config.mat ...')
-save(PathVariables.configPath, 'PublishOptions', ... 
-    '-append', '-nocompression');
-
-% save FigureOptions and TileOptions structs to config.mat with no compression,
-% append to file
-disp('Save figure and tile options configuration to config.mat ...')
-save(PathVariables.configPath, 'FigureOptions', 'TileOptions', ... 
-    '-append', '-nocompression');
