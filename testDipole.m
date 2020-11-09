206725cb73ca41a748d1e0823deb1b54f4972fcb
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2020-11-09 12:47:12 +0100

    Update documentation after correction. Rerun config build, due to work from another machine.

--- testDipole.m
+++ testDipole.m
@@ -1,8 +1,9 @@
 % sensor array geometry, distance to dipole and size
-nSensors = 8;
+nSensors = 1;
 sensorArraySize = 2;
 sensorArrayHalfSize = sensorArraySize / 2;
 sensorArrayResolution = sensorArraySize / (nSensors -1);
+sensorArray
 
 % sphere radius arround dipole in mm
 sphereRadius = 2;
@@ -16,6 +17,8 @@ zPosition = 0;
 X = meshgrid(-sensorArrayHalfSize:sensorArrayResolution:sensorArrayHalfSize) - xPosition;
 Y = meshgrid(-sensorArrayHalfSize:sensorArrayResolution:sensorArrayHalfSize) - yPosition;
 Z = zeros(nSensors, nSensors) - sphereRadius - zPosition;
+
+X=0;Y=0;Z=-2;
 
 % position of each sensor dot reshape grid to position radius 3xN^2 x,y,z column
 R = [X(:), Y(:), Z(:)]';
@@ -33,7 +36,7 @@ HAmp = 8.477e3;
 % the moment amplitude to huge value to prevent numeric failure
 mAmp = 1e6;
 % number of angles to observe, even from 0 to 360 degree
-nTheta = 8;
+nTheta = 720;
 % tilt angle in z-axes
 phi = 0;
 % angele resolution of rotation (full scale)
@@ -46,13 +49,18 @@ thetaResolution = 0.5;
 % https://en.wikipedia.org/wiki/Unit_vector
 % calculate h-field with unit vector of r to simplify the field formular
 H = zeros(3, nSensors^2);
+Habs = zeros(nTheta, nSensors^2);
 Hx = zeros(nSensors, nSensors, nTheta);
 Hy = zeros(nSensors, nSensors, nTheta);
 Hz = zeros(nSensors, nSensors, nTheta);
 for i = 1:nTheta
     % calculate H-field of one rotation step for all positions, this allows
     % singel positions too
-    H = (3 * Runit .* (M(:,1)' * Runit) - M(:,1)) / 4 / pi ./ Rabs.^3;
+    H = (3 * Runit .* (M(:,i)' * Runit) - M(:,i)) / 4 / pi ./ Rabs.^3;
+    % caculate the absolute magnitute of H-field components for later
+    % filter the maximum magnitude to scale the field components to wished
+    % field strenght
+    Habs(i,:) = sqrt(sum(H.^2, 1));
     % separate parts or field in axes direction/ components
     Hx(:,:,i) = reshape(H(1,:), nSensors, nSensors);
     Hy(:,:,i) = reshape(H(2,:), nSensors, nSensors);
@@ -63,6 +71,5 @@ end
 % last rotation part but because the dipole decribes a circle roation and the
 % geoemetry of the sensor array is a square and even distributed it is allowed
 % to get it from on rotation instead over all.
-Habs = sqrt(sum(H.^2, 1));
 HabsMax = max(Habs,[],'all');
 
