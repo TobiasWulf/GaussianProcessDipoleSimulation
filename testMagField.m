cd28f0a76376b9709d943eb9efb18a12a5c4aa01
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2020-11-15 06:03:04 +0100

    Add text for poles to plot

--- testMagField.m
+++ testMagField.m
@@ -33,7 +33,7 @@ set(gcf,'renderer','zbuffer')
 % surf(X, Z, atan2(HxDx ./Habs, HxDx ./ Habs))
 % surf(X, Z, HxDz ./ Habs)
 % surf(X, Z, Habs)
-im = imagesc(x, z, mag2db(Habs), 'AlphaData', 0.6);
+im = imagesc(x, z, mag2db(Habs), 'AlphaData', 1);
 set(gca, 'YDir', 'normal');
 colormap('jet');
 cb = colorbar;
@@ -44,7 +44,7 @@ view(2)
 hold on
 grid on
 st = streamslice(X, Z, Hx, Hz, 'noarrows', 'cubic');
-% set(st, 'Color', 'k');
+set(st, 'Color', 'k');
 
 idx = 10:10:200-10;
 q = quiver(X(idx, idx), Z(idx, idx), Hx(idx, idx), Hz(idx, idx), 0.5, 'k');
@@ -55,6 +55,8 @@ p = pi/2:0.01:3*pi/2;
 semicrc = r.*[cos(p); sin(p)];
 patch(semicrc(1,:), semicrc(2,:),'r')
 patch(-semicrc(1,:), -semicrc(2,:),'g')
+text(-0.001, 0, 'N', 'FontSize', 18)
+text(0.0005, 0, 'S', 'FontSize', 18)
 xlim(gca, [-0.012 0.012])
 ylim(gca, [-0.012 0.012])
 axis equal
