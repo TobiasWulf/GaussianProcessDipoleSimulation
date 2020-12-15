d2f179ede339de17ccd5b60d29ff26b22cf4f682
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2020-12-15 09:42:40 +0100

    Update Unit Tests part of the html documentation.

--- docs/Unit_Tests.m
+++ docs/Unit_Tests.m
@@ -1,4 +1,49 @@
 %% Unit Tests
+% Unit Tests are provideing way to test core functionallity of the written
+% software components. Matlab supports various methods to apply Unit Tests.
+% The designed tests are using script-based testing. So for each function
+% or functionallity needs to be tested a own test script is written and
+% gathered into a main test script where all standalone test scripts are
+% combined to a test suite and executed at once.
+%
+%
+%% runTests
+% Test suite script which executes all Unit Tests scripts at once and
+% gathers the test results in a Matlab table.
+%
+%
+%% removeFilesFromDirTest
+% Test of function removeFilesFromDir. Creates several files and
+% directories and deletes them during testing.
+%
+%
+%% rotate3DVectorTest
+% Test rotate3DVector function. Do some rotations and check results.
+%
+%
+%% generateDipoleRotationMomentsTest
+% Test the generation of magnetic dipole moments for a full rotation
+% between 0° and 360°.
+%
+%
+%% generateSensorArraySquareGridTest
+% Test the meshgrid generation of the sensor array and shifting it in x and
+% y direction.
+%
+%
+%% computeDipoleH0NormTest
+% Test magnetic field norming function. Simple test of consitent data.
+%
+%
+%% computeDipoleHFieldTest
+% Test the magnetic dipole equation to generate dipole fields in 3D
+% meshgrid of data points. Test field characteristics like symmetry and so
+% on.
+%
+%
+%% tiltRotationTest
+% Test tilt rotation of a dipole magnetic. Tilt magnet and coordinate cross
+% to fetch pole values during rotation.
 %
 %
 %% Requirements
