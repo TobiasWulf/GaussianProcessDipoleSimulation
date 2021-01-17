2eb7ee6eb090fb1c067d51f4dc912a7fd1114c62
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2021-01-17 19:07:47 +0100

    save inbetween.

--- temp/scratch4.m
+++ temp/scratch4.m
@@ -29,7 +29,8 @@ P = trainDS.Info.SensorArrayOptions.SensorCount;
 N = trainDS.Info.UseOptions.nAngles;
 
 % voltages, offset and bridge outputs and norm on angle resolution
-beta = D / trainDS.Data.angleStep;
+beta1 = D / trainDS.Data.angleStep;
+beta2 = D / trainDS.Data.angleStep;
 off = trainDS.Info.SensorArrayOptions.Voff;
 Xcos = trainDS.Data.Vcos;
 Xsin = trainDS.Data.Vsin;
@@ -37,8 +38,14 @@ Xsin = trainDS.Data.Vsin;
 % angles in training and test data (column vectors)
 ydeg = trainDS.Data.angles';
 yrad = ydeg * pi/180;
-ycos = cos(yrad);
-ysin = sin(yrad);
+switch trainDS.Info.UseOptions.BaseReference
+    case 'KMZ60'
+        pf = 2;
+    otherwise
+        pf = 1;
+end
+[ysin, ycos] = angles2sinoids(yrad, true, 1, pf);
+ysin = ysin'; ycos = ycos';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -79,10 +86,11 @@ logDetKy = 2 * sum(log(diag(L)));
 % alphaSin = L' \ (L \ ysin);
 
 opts1.LT = true;
-opts2.UT = false;
-alphaCos = linsolve(L', linsolve(L, ycos - squeeze(mean(Xcos, 2) - off)' * ones(D,1) * beta, opts1), opts2);
-alphaSin = linsolve(L', linsolve(L, ysin - squeeze(mean(Xsin, 2) - off)' * ones(D,1) * beta, opts1), opts2);
-alphaTan = linsolve(L', linsolve(L, unwrap(atan2(ysin, ycos)) - unwrap(atan2(squeeze(mean(Xsin, 2) - off)' * ones(D,1) * beta, squeeze(mean(Xcos, 2) - off)' * ones(D,1) * beta)), opts1), opts2);
+opts2.UT = true;
+alphaCos = linsolve(L', linsolve(L, ycos - squeeze(mean(Xcos, 2) - off)' * ones(D,1) * beta1, opts1), opts2);
+alphaSin = linsolve(L', linsolve(L, ysin - squeeze(mean(Xsin, 2) - off)' * ones(D,1) * beta2, opts1), opts2);
+alphaTan = linsolve(L', linsolve(L, atan2(ysin, ycos) - atan2(squeeze(mean(Xsin, 2) - off)' * ones(D,1) * beta2, squeeze(mean(Xcos, 2) - off)' * ones(D,1) * beta1), opts1), opts2);
+
 % alphaCos = R \ (R' \ ycos);
 % alphaSin = R \ (R' \ ysin);
 
@@ -96,9 +104,9 @@ alphaTan = linsolve(L', linsolve(L, unwrap(atan2(ysin, ycos)) - unwrap(atan2(squ
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Compute log marginal likelihood for cosine and sine prediction each depended
 % on the alpha weights.
-lmllCos = -0.5 * ((ycos - squeeze(mean(Xcos, 2) - off)' * ones(D,1) * beta)' * alphaCos + logDetKy + N * log(2 * pi));
-lmllSin = -0.5 * ((ysin - squeeze(mean(Xsin, 2) - off)' * ones(D,1) * beta)' * alphaSin + logDetKy + N * log(2 * pi));
-lmllTan = -0.5 * ((unwrap(atan2(ysin, ycos)) - unwrap(atan2(squeeze(mean(Xsin, 2) - off)' * ones(D,1) * beta, squeeze(mean(Xcos, 2) - off)' * ones(D,1) * beta)))' * alphaSin + logDetKy + N * log(2 * pi));
+lmllCos = -0.5 * ((ycos - squeeze(mean(Xcos, 2) - off)' * ones(D,1) * beta1)' * alphaCos + logDetKy + N * log(2 * pi));
+lmllSin = -0.5 * ((ysin - squeeze(mean(Xsin, 2) - off)' * ones(D,1) * beta2)' * alphaSin + logDetKy + N * log(2 * pi));
+lmllTan = -0.5 * ((atan2(ysin, ycos) - atan2(squeeze(mean(Xsin, 2) - off)' * ones(D,1) * beta2, squeeze(mean(Xcos, 2) - off)' * ones(D,1) * beta1))' * alphaTan + logDetKy + N * log(2 * pi));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -125,9 +133,9 @@ for n = 1:Ntest
         [sigma2F, sigmaL]);
     
     % compute predictive mean for cosine and sine
-    fMeanCos(n) = (mean(XtestCos, 1) - off) * ones(D,1) * beta + kx' * alphaCos;
-    fMeanSin(n) = (mean(XtestSin, 1) - off) * ones(D,1) * beta + kx' * alphaSin;
-    hTbeta = atan2((mean(XtestSin, 1) - off) * ones(D,1) * beta, (mean(XtestCos, 1) - off) * ones(D,1) * beta);
+    fMeanCos(n) = (mean(XtestCos, 1) - off) * ones(D,1) * beta1 + kx' * alphaCos;
+    fMeanSin(n) = (mean(XtestSin, 1) - off) * ones(D,1) * beta2 + kx' * alphaSin;
+    hTbeta = atan2((mean(XtestSin, 1) - off) * ones(D,1) * beta2, (mean(XtestCos, 1) - off) * ones(D,1) * beta1);
     fMeanTan(n) =  hTbeta+ kx' * alphaTan;
 
     % compute predictive variance
@@ -140,16 +148,57 @@ for n = 1:Ntest
 end
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+H = featureFunction(Xcos, Xsin);
 
 
-%VcosTest = testDS.Data.Vcos;
-%VsinTest = testDS.Data.Vsin;
 
-%refTestDeg = testDS.Data.angles';
-%refTestRad = refTestDeg * pi/180;
-%refTestCos = cos(refTestRad);
-%refTestSin = sin(refTestRad);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Compute the product of an inverted matrix K^-1 and a vector y to get a weight
+% vector alpha by solving the linear system by cholesky decomposed lower
+% triangle matrix L of matrix K.
+function alpha = computeInverseMatrixProduct(L, y)
+    opts1.LT = true;
+    opts2.UT = true;
+    alpha = linsolve(L, y, opts1);
+    alpha = linsolve(L', alpha , opts2);
+end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Compute the Cholesky decomposition of a symmetrix positive definite matrix A
+% and calculate the log determinate as side product of the decomposition. 
+% Compute the lower triangle matrix L.
+function [L, logDet] = cholDecomposeA2L(A)
+    [L, flag] = chol(A, 'lower');
+    assert(flag == 0);
+    logDet = 2 * sum(log(diag(L)));
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Mean function to compute the H matrix as set of h(x) vectors for each
+% predictor to apply a mean feature space of atan2 function.
+function H = featureFunction(Xcos, Xsin)
+    assert(all(size(Xcos) == size(Xsin)));
+    [D1, D2, N] = size(Xcos);
+    H = zeros(D1 * D2, N);
+    for n = 1:N
+        h = atan2(Xsin(:,:,n), Xcos(:,:,n));
+        H(:, n) = h(:);
+    end
+    H = [ones(1,N); H];
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Add noise to covarianc matrix for noisy observations. Add noise along matrix
+% diagonal.
+function Ky = addNoise2Covariance(K, sigma2N)
+    Ky = K + sigma2N * eye(size(K));
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -180,7 +229,7 @@ end
 % (applicationt) it computes the MxN matrix K for test inputs of size DxDxM
 % and training points size of DxDxN. If it is a single test point so DxDx1
 % matrix the function computes the covariance vector of size 1xN of the
-% test to each training observation. 
+% test to each training observation. Computes noise free covariances.
 function K = quadraticFrobeniusCovariance(XcosM, XcosN, XsinM, XsinN, params)
     arguments
         % validate matrices as real numeric 3D matrices of equal sizes
@@ -219,6 +268,122 @@ function K = quadraticFrobeniusCovariance(XcosM, XcosN, XsinM, XsinN, params)
             % engage lengthscale and variance on distance
             K(m,n) = sigma2F / (sigmaL + r2);
             
+        end
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% helper function to convert angles (rad or degree) to sinus and cosinus waves
+% using built in functions cos, and sin, compute with amplitude scale
+% factor and which gives the posibility to scale to one, additional returns
+% angles in rad if passed in degree and differences to sinus and cosinus if
+% compare vectors for each is passed, it is also posible to multiple the periods
+% of given angles by period factor.
+% varargout 1: angles in rad
+% varargout 2: sinus diff
+% varargout 3: cosinus diff
+function [sinus, cosinus, varargout] = angles2sinoids(angles, rad, amp, pf, namedargs)
+    arguments
+        % validate angles and origins as row vectors of same length
+        angles (1,:) double {mustBeVector, mustBeReal}
+        % validate rad option flag as boolean with default true
+        rad (1,1) logical {mustBeNumericOrLogical} = true
+        % validate amplitude vector as real scalar factor with default 1
+        amp (1,1) double {mustBeReal} = 1
+        % validate period factor as real, positive scalar with default 1
+        pf (1,1) double {mustBePositive} = 1
+        namedargs.sinus (1,:) double {mustBeReal, mustBeEqualSize(angles, namedargs.sinus)}
+        namedargs.cosinus (1,:) double {mustBeReal, mustBeEqualSize(angles, namedargs.cosinus)}
+    end
+    
+    % if rad flag is false and angles in degree convert to rad
+    if ~rad, angles = angles * pi / 180; end
+    
+    % calculate sinoids
+    sinus = amp * sin(pf * angles);
+    cosinus = amp * cos(pf * angles);
+    
+    % angles in rad
+    if nargout > 2
+        varargout{1} = angles;
+    end
+    % sinus difference
+    if nargout > 3
+        if isfield(namedargs, 'sinus')
+            varargout{2} = diff([sinus; namedargs.sinus], 1, 1);
+        else
+            varargout{2} = NaN(1, length(angles));
+        end
+    end
+    % cosinus difference
+    if nargout > 4
+        if isfield(namedargs, 'cosinus')
+            varargout{3} = diff([cosinus; namedargs.cosinus], 1, 1);
+        else
+            varargout{3} = NaN(1, length(angles));
+        end
+    end
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% helper function to convert sinus and cosinus values to rad or degree angles
+% in compare to origin angular reference, atan2 built and correct negative
+% angles along 2pi scale to unwrap atan2 result respective to full rotation
+% returns angles in rad and converts origin from degrees to rad if needed
+% Origin: Jünemann, Added: Wulf (argument validation, rad flag)
+% namedargs: origin: origin angles
+% varargout 1: anglesDiff
+% varargout 2: originRad
+function [angles, varargout] = sinoids2angles(sinus, cosinus, rad, pf, namedargs)
+    arguments
+        % validate sinus, cosinus and origin as row vectors of the same length
+        sinus (1,:) double {mustBeVector, mustBeReal}
+        cosinus (1,:) double {mustBeReal, mustBeEqualSize(sinus, cosinus)}
+        % validate rad option flag as boolean with default true
+        rad (1,1) logical {mustBeNumericOrLogical} = true
+        % validate period factor as real, positive scalar with default 1
+        pf (1,1) double {mustBePositive} = 1
+        % validate angles origin as vector of same length as sinoids
+        namedargs.origin (1,:) double {mustBeReal, mustBeEqualSize(cosinus, namedargs.origin)}
+    end
+    
+    % convert sinoids vector componets to rad angles, atan2 provides angles
+    % btween 0° and 180° and abstracts angles between 180° and  360° to negative
+    % quadrant from -180 to 0°
+    angles = atan2(sinus, cosinus);
+    
+    % correct angles from 0° to 360° (0 to 2pi) with period factor if multiple
+    % periods abstarcts angles between 0° and 360°
+    angles = unwrap(angles) / pf;
+    
+        
+    % calculate difference to origin angular reference
+    if nargout > 1
+        if isfield(namedargs, 'origin')
+            % convert to rad if rad flag is false
+            if ~rad, namedargs.origin = namedargs.origin * pi / 180; end
+            % claculate difference
+            anglesDiff = diff([namedargs.origin; angles], 1, 1);
+            % ensure calculated difference matches interval
+            idx = anglesDiff > pi;
+            anglesDiff(idx) = anglesDiff(idx) - 2 * pi;
+            idx = anglesDiff < -pi;
+            anglesDiff(idx) = anglesDiff(idx) + 2 * pi;
+            varargout{1} = anglesDiff;
+        else
+            varargout{1} = NaN(1, length(angles));
+        end
+    end
+    
+    % origin in rad if passed in degree, converted in nargout > 1, ~rad
+    if nargout > 2
+        if isfield(namedargs, 'origin')
+            varargout{2} = namedargs.origin;
+        else
+            varargout{2} = NaN(1, length(angles));
         end
     end
 end
