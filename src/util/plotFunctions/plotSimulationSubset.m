70a691f37250f038bb1a7397b197e03eba76ada5
Tobias Wulf <46107549+TobiasWulf@users.noreply.github.com>
2021-04-06 15:52:26 +0200

    work plots fit latex

--- src/util/plotFunctions/plotSimulationSubset.m
+++ src/util/plotFunctions/plotSimulationSubset.m
@@ -41,7 +41,7 @@
 %
 % Created on November 29. 2020 by Tobias Wulf. Copyright Tobias Wulf 2020.
 %
-% <html>
+% <html>n
 % <!--
 % Hidden Clutter.
 % Edited on Month DD. YYYY by Editor: Single line description.
@@ -79,7 +79,7 @@ function plotSimulationSubset()
         fprintf('%s\t:\t(%d)\n', allDatasets(i).name, i)
     end
     % get numeric user input to indicate which dataset to plot
-    iDataset = input('Type number to choose dataset to plot to: ');
+    iDataset = 3;%input('Type number to choose dataset to plot to: ');
         
     % load dataset and ask user which one and how many angles %%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -91,21 +91,21 @@ function plotSimulationSubset()
         fprintf('Detect %d x %d sensors in dataset ...\n', ...
             ds.Info.SensorArrayOptions.dimension, ...
             ds.Info.SensorArrayOptions.dimension);
-        xIdx = input("Enter x indices in []: ");
-        yIdx = input("Enter y indices in []: ");
+        xIdx = input("Enter row indices in []: ");
+        yIdx = input("Enter col indices in []: ");
         if length(xIdx) ~= length(yIdx)
             error('Indices must have the same length!')
         end
-        fprintf('Detect %d angles in dataset ...\n', ...
-            ds.Info.UseOptions.nAngles);
-        nSubAngles = input('How many angles to you wish to plot: ');
-        % indices for data to plot, get sample distance for even distance
-        sampleDistance = length(downsample(ds.Data.angles, nSubAngles));
-        % get subset of angles
-        subAngles = downsample(ds.Data.angles, sampleDistance);
-        nSubAngles = length(subAngles); % just ensure
-        % get indices for subset data
-        angleIdx = find(ismember(ds.Data.angles, subAngles));    
+%         fprintf('Detect %d angles in dataset ...\n', ...
+%             ds.Info.UseOptions.nAngles);
+%         nSubAngles = input('How many angles to you wish to plot: ');
+%         % indices for data to plot, get sample distance for even distance
+%         sampleDistance = length(downsample(ds.Data.angles, nSubAngles));
+%         % get subset of angles
+%         subAngles = downsample(ds.Data.angles, sampleDistance);
+%         nSubAngles = length(subAngles); % just ensure
+%         % get indices for subset data
+%         angleIdx = find(ismember(ds.Data.angles, subAngles));    
     catch ME
         rethrow(ME)
     end
@@ -119,39 +119,24 @@ function plotSimulationSubset()
     fig = figure('Name', 'Sensor Array', ...
         'NumberTitle' , 'off', ...
         'WindowStyle', 'normal', ...
-        'MenuBar', 'none', ...
-        'ToolBar', 'none', ...
-        'Units', 'centimeters', ...
-        'OuterPosition', [0 0 37 29], ...
-        'PaperType', 'a4', ...
-        'PaperUnits', 'centimeters', ...
-        'PaperOrientation', 'landscape', ...
-        'PaperPositionMode', 'auto', ...
-        'DoubleBuffer', 'on', ...
-        'RendererMode', 'manual', ...
-        'Renderer', 'painters');
+        'WindowState', 'maximized');
     
-    tdl = tiledlayout(fig, 3, 4, ...
+    tdl = tiledlayout(fig, 4, 6, ...
         'Padding', 'compact', ...
         'TileSpacing' , 'compact');
     
     
-    title(tdl, 'Sensor Array Simulation', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 18, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    disp('Sensor Array Simulation');
     
-    subline1 = "Sensor Array (%s) of $%d\\times%d$ sensors," + ...
-        " an edge length of $%.1f$ mm, a rel. pos. to magnet surface of";
-    subline2 = " $(%.1f, %.1f, -(%.1f))$ in mm, a magnet tilt" + ...
-        " of $%.1f^\\circ$, a sphere radius of $%.1f$ mm, a imprinted";
-    subline3 = "field strength of $%.1f$ kA/m at $%.1f$ mm from" + ...
-        " sphere surface in z-axis, $%d$ rotation angles with a ";
-    subline4 = "step width of $%.1f^\\circ$ and a resolution" + ...
-        " of $%.1f^\\circ$. Visualized is a subset of $%d$ angles in ";
-    subline5 = "sample distance of $%d$ angles. Based on %s" + ...
-        " characterization reference %s.";
+    subline1 = "Sensor Array (%s) of %dx%d sensors," + ...
+        " an edge length of %.1f mm, a rel. pos. to magnet surface of";
+    subline2 = " (%.1f, %.1f, -(%.1f)) in mm, a magnet tilt" + ...
+        " of %.1f°, a sphere radius of %.1f mm, a imprinted";
+    subline3 = "field strength of %.1f kA/m at %.1f mm from" + ...
+        " sphere surface in z-axis, %d rotation angles with a ";
+    subline4 = "step width of %.1f° and a resolution" + ...
+        " of %.1f°. Visualized is a subset.";
+    subline5 = "Based on %s characterization reference %s.";
     sub = [sprintf(subline1, ...
                    ds.Info.SensorArrayOptions.geometry, ...
                    ds.Info.SensorArrayOptions.dimension, ...
@@ -169,18 +154,12 @@ function plotSimulationSubset()
                    ds.Info.UseOptions.nAngles); ...
            sprintf(subline4, ...
                    ds.Data.angleStep, ...
-                   ds.Info.UseOptions.angleRes, ...
-                   nSubAngles)
-           sprintf(subline5, ...
-                   sampleDistance, ...
+                   ds.Info.UseOptions.angleRes)
+           sprintf(subline5,...
                    ds.Info.CharData, ...
                    ds.Info.UseOptions.BridgeReference)];
     
-    subtitle(tdl, sub, ...
-        'FontWeight', 'normal', ...
-        'FontSize', 14, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    disp(sub);
     
     % get subset of needed data to plot, only one load %%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -190,10 +169,16 @@ function plotSimulationSubset()
     Z = ds.Data.Z;
     
     % calc limits of plot 1
-    maxX = ds.Info.UseOptions.xPos + ds.Info.SensorArrayOptions.edge;
-    maxY = ds.Info.UseOptions.yPos + ds.Info.SensorArrayOptions.edge;
-    minX = ds.Info.UseOptions.xPos - ds.Info.SensorArrayOptions.edge;
-    minY = ds.Info.UseOptions.yPos - ds.Info.SensorArrayOptions.edge;
+    a= ds.Info.SensorArrayOptions.edge;
+    maxX = ds.Info.UseOptions.xPos + a * 0.66;
+    maxY = ds.Info.UseOptions.yPos + a * 0.66;
+    minX = ds.Info.UseOptions.xPos - a * 0.66;
+    minY = ds.Info.UseOptions.yPos - a * 0.66;
+    dp = a / (ds.Info.SensorArrayOptions.dimension - 1);
+    x1 = ds.Info.UseOptions.xPos - a/2;
+    x2 = ds.Info.UseOptions.xPos + a/2;
+    y1 = ds.Info.UseOptions.yPos - a/2;
+    y2 = ds.Info.UseOptions.yPos + a/2;
     
     % calculate colormap to identify scatter points
     c=zeros(N,N,3);
@@ -214,17 +199,17 @@ function plotSimulationSubset()
     
     % plot sensor grid in x and y coordinates and constant z layer %%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
-    ax1 = nexttile(1);
+    ax1 = nexttile(6,[2 1]);
     % plot each cooredinate in loop to create a special shading constant
     % reliable to orientation for all matrice
     hold on;
     
-    scatter(X(:), Y(:), [], [0.8 0.8 0.8], 'filled', ...
+    scatter(X(:), Y(:), 48, [0.8 0.8 0.8], 'filled', ...
         'MarkerEdgeColor', 'k', 'LineWidth', 0.8);
     
     for k = 1:length(xIdx)
         i = xIdx(k); j = yIdx(k);
-        scatter(X(i,j), Y(i,j), [], [R(i,j), G(i,j), B(i,j)], 'filled', ...
+        scatter(X(i,j), Y(i,j), 96, [R(i,j), G(i,j), B(i,j)], 'filled', ...
             'MarkerEdgeColor', 'k', 'LineWidth', 0.8);
     end
        
@@ -234,66 +219,23 @@ function plotSimulationSubset()
     grid on;
     xlim([minX maxX]);
     ylim([minY maxY]);
+    xticks(x1:dp:x2);
+    xticklabels(1:ds.Info.SensorArrayOptions.dimension);
+    yticks(y1:dp:y2);
+    yticklabels(ds.Info.SensorArrayOptions.dimension:-1:1);
     
-    % text and labels
-    text(minX+0.2, minY+0.2, ...
-        sprintf('$Z = %.1f$ mm', Z(1)), ...
-        'Color', 'k', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    xlabel('$j$');
     
-    xlabel('$X$ in mm', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    ylabel('$i$');
     
-    ylabel('$Y$ in mm', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
-    
-    title(sprintf('Sensor Array $%d\\times%d$', N, N), ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    title(sprintf('c) Sensor Array $%d\\times%d$', N, N));
     
     hold off;
     
-    % plot rotation angles in polar view %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    nexttile(2);
-    % plot all angles grayed out
-    polarscatter(ds.Data.angles/180*pi, ones(1, ds.Info.UseOptions.nAngles), ...
-        5, [0.8 0.8 0.8], 'filled');
-    
-    % radius ticks and label
-    rticks(1);
-    rticklabels("");
-    hold on;
-    
-    % plot subset of angles
-    polarscatter(subAngles/180*pi, ones(1, nSubAngles), 5, 'b', 'filled');
-    ax2 = gca;    
-        
-    % axis shape
-    axis tight;
-
-    % text an labels
-    title('Rotation around Z-Axis in Degree', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
-
-    hold off;
     
     % Cosinus bridge outputs for rotation step %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    ax3 = nexttile(3);
+    ax3 = nexttile(1, [2 2]);
     hold on;
     
     % set colormap
@@ -311,29 +253,23 @@ function plotSimulationSubset()
     grid on;
         
     % test and labels
-    xlabel('$H_x$ in kA/m', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    xlabel('$H_x$ in kA/m');
     
-    ylabel('$H_y$ in kA/m', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    ylabel('$H_y$ in kA/m');
     
-    title('$V_{cos}(H_x, H_y)$', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    title('a) $V_{cos}(H_x, H_y)$');
     
+    % add colorbar and place it
+    cb1 = colorbar;
+    cb1.Label.String = 'in V';
+    cb1.TickLabelInterpreter = 'latex';
+    cb1.Label.Interpreter = 'latex';
+    cb1.Label.FontSize = 20;
     hold off;
     
     % Sinus bridge outputs for rotation step %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    ax4 = nexttile(4);
+    ax4 = nexttile(13, [2 2]);
     hold on;
     
     % set colormap
@@ -351,92 +287,57 @@ function plotSimulationSubset()
     grid on;
     
     % test and labels
-    xlabel('$H_x$ in kA/m', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    xlabel('$H_x$ in kA/m');
     
-    ylabel('$H_y$ in kA/m', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    ylabel('$H_y$ in kA/m');
     
-    title('$V_{sin}(H_x, H_y)$', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    title('d) $V_{sin}(H_x, H_y)$');
     
     % add colorbar and place it
     cb2 = colorbar;
     cb2.Label.String = 'in V';
+    cb2.TickLabelInterpreter = 'latex';
     cb2.Label.Interpreter = 'latex';
-    cb2.Label.FontSize = 12;
+    cb2.Label.FontSize = 20;
     
     hold off;
     
     % plot Vcos Vsin over angles %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % axes limits
-    xlimits = [-10 370];
+    xlimits = [0 360];
     ylimits = [min(cat(...
         3, ds.Data.VsinRef, ds.Data.VcosRef), [], 'all') - 0.1*Vcc, ...
         max(cat(3, ds.Data.VsinRef, ds.Data.VcosRef), [], 'all') + 0.1*Vcc];
     
     % Vcos
-    ax5 = nexttile([1 4]);
-    yline(Voff, 'k-.', 'LineWidth', 1.2);
+    ax5 = nexttile(3, [2 3]);
+    yline(Voff, 'k-.', 'LineWidth', 2.5);
     xlim(xlimits);
     ylim(ylimits);
     grid on;
     
-    xlabel('$\theta$ in Degree', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    xlabel('$\alpha$ in $^\circ$');
     
-    ylabel('$V{cos}(\theta)$ in V', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    %ylabel('in V');
     
     title(sprintf(...
-        "$V_{cos}$ of Enabled Array Positions over $\\theta$," + ...
-        " $V_{cc} = %.1f$ V, $V_{off} = %.2f$ V", Vcc, Voff), ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+        "b) $V_{cos}(\\alpha)$ f." + ...
+        " $V_{cc} = %.1f$ V, $V_{off} = %.2f$ V", Vcc, Voff));
         
     % Vsin
-    ax6 = nexttile([1 4]);
-    yline(Voff, 'k-.', 'LineWidth', 1.2);
+    ax6 = nexttile(15, [2 3]);
+    yline(Voff, 'k-.', 'LineWidth', 2.5);
     xlim(xlimits);
     ylim(ylimits);
     grid on;
     
-    xlabel('$\theta$ in Degree', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    xlabel('$\alpha$ in $^\circ$');
     
-    ylabel('$V{sin}(\theta)$ in V', ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    %ylabel('in V');
     
-    title(sprintf("$V_{sin}$ of Enabled Array Positions over" + ...
-        " $\\theta$, $V_{cc} = %.1f$ V, $V_{off} = %.2f$ V", Vcc, Voff), ...
-        'FontWeight', 'normal', ...
-        'FontSize', 12, ...
-        'FontName', 'Times', ...
-        'Interpreter', 'latex');
+    title(sprintf("e) $V_{sin}(\\alpha)$ f." + ...
+        " $V_{cc} = %.1f$ V, $V_{off} = %.2f$ V", Vcc, Voff));
     
     % loop through subset of dataset and renew plots %%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -450,21 +351,21 @@ function plotSimulationSubset()
     for k = 1:length(xIdx)
         i = xIdx(k); j = yIdx(k);
         % H load subset
-        Hx = squeeze(ds.Data.Hx(i,j,angleIdx));
-        Hy = squeeze(ds.Data.Hy(i,j,angleIdx));
+        Hx = squeeze(ds.Data.Hx(i,j,:));
+        Hy = squeeze(ds.Data.Hy(i,j,:));
         % get min max
 
         % load V subset
-        Vcos = squeeze(ds.Data.Vcos(i,j,angleIdx));
-        Vsin = squeeze(ds.Data.Vsin(i,j,angleIdx));
+        Vcos = squeeze(ds.Data.Vcos(i,j,:));
+        Vsin = squeeze(ds.Data.Vsin(i,j,:));
 
         % update plot 3, 4, 5 and 6
-        scatter(ax3, Hx, Hy, 1, [R(i,j), G(i,j), B(i,j)] , 'filled');
-        scatter(ax4, Hx, Hy, 1, [R(i,j), G(i,j), B(i,j)], 'filled');
-        scatter(ax5, subAngles, Vcos, 12, [R(i,j), G(i,j), B(i,j)] , ...
-            'filled', 'MarkerEdgeColor', 'k', 'LineWidth', 0.5);
-        scatter(ax6, subAngles, Vsin, 12, [R(i,j), G(i,j), B(i,j)] , ...
-            'filled', 'MarkerEdgeColor', 'k', 'LineWidth', 0.5);
+        scatter(ax3, Hx, Hy, 5, [R(i,j), G(i,j), B(i,j)] , 'filled');
+        scatter(ax4, Hx, Hy, 5, [R(i,j), G(i,j), B(i,j)], 'filled');
+        scatter(ax5, ds.Data.angles, Vcos, 8, [R(i,j), G(i,j), B(i,j)] , ...
+            'filled');
+        scatter(ax6, ds.Data.angles, Vsin, 8, [R(i,j), G(i,j), B(i,j)] , ...
+            'filled');
     end
     
     % release plots    
@@ -476,18 +377,18 @@ function plotSimulationSubset()
     % save figure to file %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % get file path to save figure with angle index
-    [~, fName, ~] = fileparts(ds.Info.filePath);
-    
-    % save to various formats
-    yesno = input('Save? [y/n]: ', 's');
-    if strcmp(yesno, 'y')
-        fLabel = input('Enter file label: ', 's');
-        fName = fName + "_SubsetPlot_" + fLabel;
-        savefig(fig, fullfile(fPath, fName));
-        print(fig, fullfile(fPath, fName), '-dsvg');
-        print(fig, fullfile(fPath, fName), '-depsc', '-tiff', '-loose');
-        print(fig, fullfile(fPath, fName), '-dpdf', '-loose', '-fillpage');
-    end
-    close(fig);
+%     [~, fName, ~] = fileparts(ds.Info.filePath);
+%     
+%     % save to various formats
+%     yesno = input('Save? [y/n]: ', 's');
+%     if strcmp(yesno, 'y')
+%         fLabel = input('Enter file label: ', 's');
+%         fName = fName + "_SubsetPlot_" + fLabel;
+%         savefig(fig, fullfile(fPath, fName));
+%         print(fig, fullfile(fPath, fName), '-dsvg');
+%         print(fig, fullfile(fPath, fName), '-depsc', '-tiff', '-loose');
+%         print(fig, fullfile(fPath, fName), '-dpdf', '-loose', '-fillpage');
+%     end
+%     close(fig);
 end
 
